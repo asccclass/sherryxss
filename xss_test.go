45cc8cddb4df5b198808c7fad88252de0b72086f
justgps <justgps@gmail.com>
2019-05-12 23:29:56 -0700

    增加測試函數

--- xss_test.go
+++ xss_test.go
@@ -2,19 +2,33 @@ package SherryXSS
 
 import (
    "testing"
+   "fmt"
+   // "reflect"
 )
 
+func TestXSS(t *testing.T) {
+   want := XX{IntField:42, StringField:"&lt;script&gt;alert(&#39;foo&#39;);&lt;/script&gt;"}
+   test := XX{IntField:42, StringField: "<script>alert(\"foo\");</script>"}
 
-func TestXSS() {
-   var want string = `struct { IntField int; StringField string }{IntField:42, StringField:"&lt;script&gt;alert(&#39;foo&#39;);&lt;/script&gt;"}`
-
-   xs := XSS{IntField:42, Intfield: "<script>alert("foo");</script>"}}
-   s, err := xs.HtmlEscape(myStruct)
+   xs := XSS{}
+   s, err := xs.HtmlEscape(test)
 
    if err != nil {
-      fmt.Printf("Error:%v\n", err)
-   } else if xs["IntField"] != want["IntField"] || xs["StringField"] != want["StringField"]) {
+      fmt.Printf("Error: %v \n", err)
+   } 
+
+   vals := s.(XX)
+   
+   if vals != want {
+      t.Error("Something error...")
+   }
+
+   fmt.Printf("Wnt:%v\nGot:%v", want, vals)
+/*
+
+   if vals.Field(0).Interface().(int) != want.IntField {
       fmt.Printf("Return struct is :%v\n", s)
    }
+ //|| val(1).Interfacce().Elem().(string) != want.StringField {
+*/
 }
-_
